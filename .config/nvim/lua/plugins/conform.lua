6b9c44cc0e6f672a2eb547e6c18c98e46645a644
skai273 <kinoshita116@gmail.com>
2025-12-30 15:53:37 +0900

    add conform.nvim config

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      json = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      rust = { "rustfmt" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+  },
+}
+
