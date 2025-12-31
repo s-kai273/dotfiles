7177794b0eaae32745adade02fdd5382404bc52f
skai273 <kinoshita116@gmail.com>
2025-12-31 17:26:44 +0900

    add function to copy diagnostic message

--- .config/nvim/lua/diagnostic.lua
+++ .config/nvim/lua/diagnostic.lua
@@ -18,3 +18,30 @@ vim.api.nvim_create_autocmd("CursorMoved", {
     end))
   end,
 })
+
+local function copy_diagnostic_under_cursor()
+  local diags = vim.diagnostic.get(0, {
+    lnum = vim.fn.line(".") - 1,
+  })
+
+  if #diags == 0 then
+    vim.notify("No diagnostics", vim.log.levels.INFO)
+    return
+  end
+
+  local msg = table.concat(
+    vim.tbl_map(function(d)
+      return d.message
+    end, diags),
+    "\n"
+  )
+  vim.fn.setreg("+", msg)
+  vim.notify("Diagnostic copied")
+end
+
+vim.keymap.set(
+  "n",
+  "<leader>yd",
+  copy_diagnostic_under_cursor,
+  { desc = "Copy diagnostic under cursor" }
+)
