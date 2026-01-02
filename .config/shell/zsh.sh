8c513087d32b72d3570b495664842c4ac1902774
skai273 <kinoshita116@gmail.com>
2026-01-02 11:34:46 +0900

    change zsh keybinding to vi mode

--- .config/shell/zsh.sh
+++ .config/shell/zsh.sh
@@ -1,3 +1,6 @@
+bindkey -v
+bindkey -M viins '^E' autosuggest-accept
+
 source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
