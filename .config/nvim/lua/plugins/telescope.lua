f8058c8440cf4ba064534de312ae887f151177f1
skai273 <kinoshita116@gmail.com>
2026-01-02 22:03:37 +0900

    add diagnostic setting with nvim

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -18,27 +18,13 @@ return {
 
     cmd = "Telescope",
     keys = {
-      { "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Find files" },
-      { "<leader>fg", function() require("telescope.builtin").live_grep() end,  desc = "Live grep" },
-      { "<leader>fb", function() require("telescope.builtin").buffers() end,    desc = "Buffers" },
-      { "<leader>fh", function() require("telescope.builtin").help_tags() end,  desc = "Help" },
-      {
-        "gd",
-        function()
-          require("telescope.builtin").lsp_definitions()
-        end,
-        desc = "Go to definitions"
-      },
-      {
-        "gr",
-        function()
-          require("telescope.builtin").lsp_references({
-            include_declaration = false,
-            show_line = false,
-          })
-        end,
-        desc = "Go to references"
-      },
+      { "<leader>ff", function() require("telescope.builtin").find_files() end,                                                       desc = "Find files" },
+      { "<leader>fg", function() require("telescope.builtin").live_grep() end,                                                        desc = "Live grep" },
+      { "<leader>fb", function() require("telescope.builtin").buffers() end,                                                          desc = "Buffers" },
+      { "<leader>fh", function() require("telescope.builtin").help_tags() end,                                                        desc = "Help" },
+      { "<leader>fd", function() require("telescope.builtin").diagnostics() end,                                                      desc = "Diagnostics" },
+      { "gd",         function() require("telescope.builtin").lsp_definitions() end,                                                  desc = "Go to definitions" },
+      { "gr",         function() require("telescope.builtin").lsp_references({ include_declaration = false, show_line = false }) end, desc = "Go to references" },
     },
 
     config = function()
