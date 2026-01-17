8514c0245db535a547f3fa02e220128e488db061
skai273 <kinoshita116@gmail.com>
2026-01-17 11:48:24 +0900

    modify wezterm settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,13 +3,14 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = true
 config.color_scheme = "Tokyo Night Moon"
 config.debug_key_events = true
-config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
-config.use_ime = true
-config.warn_about_missing_glyphs = false
 config.font = wezterm.font_with_fallback({
   "JetBrains Mono",
 })
+config.font_size = 12.0
+config.hide_tab_bar_if_only_one_tab = false
+config.window_decorations = "RESIZE"
+config.use_ime = true
+config.warn_about_missing_glyphs = false
 
 config = require("lua.modal_wezterm").apply(config, wezterm)
 config = require("lua.keybinds").apply(config, wezterm)
