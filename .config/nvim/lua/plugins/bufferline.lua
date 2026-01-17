8f89b7b411637d81ce757ed0343af1c30f60184f
skai273 <kinoshita116@gmail.com>
2026-01-17 11:47:52 +0900

    add settings of bufferline to change buffer order

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -4,6 +4,14 @@ return {
   dependencies = 'nvim-tree/nvim-web-devicons',
   event = "UIEnter",
   config = true,
+  opts = function(_, opts)
+    opts.options = opts.options or {}
+    opts.options.sort_by = function(buf_a, buf_b)
+      local a = vim.fn.getbufinfo(buf_a.id)[1]
+      local b = vim.fn.getbufinfo(buf_b.id)[1]
+      return a.lastused > b.lastused
+    end
+  end,
   keys = {
     { "<Tab>",   "<Cmd>BufferLineCycleNext<CR>", desc = "Next buffer line" },
     { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous buffer line" },
