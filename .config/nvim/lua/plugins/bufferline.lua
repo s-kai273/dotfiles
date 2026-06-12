a5437833a3a22dfd9d9a710174a8b0dfc67cfc95
skai273 <kinoshita116@gmail.com>
2026-06-12 22:51:18 +0900

    update nvim settings to improve bufferline config

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -4,18 +4,16 @@ return {
   dependencies = 'nvim-tree/nvim-web-devicons',
   event = "UIEnter",
   config = true,
-  opts = function(_, opts)
-    opts.options = opts.options or {}
-    opts.options.sort_by = function(buf_a, buf_b)
-      local a = vim.fn.getbufinfo(buf_a.id)[1]
-      local b = vim.fn.getbufinfo(buf_b.id)[1]
-      return a.lastused > b.lastused
-    end
-  end,
+  opts = {
+    options = {
+      sort_by = "insert_after_current",
+    },
+  },
   keys = {
-    { "<Tab>",   "<Cmd>BufferLineCycleNext<CR>", desc = "Next buffer line" },
-    { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous buffer line" },
+    { "]b",         "<Cmd>BufferLineCycleNext<CR>",   desc = "Next buffer line" },
+    { "[b",         "<Cmd>BufferLineCyclePrev<CR>",   desc = "Previous buffer line" },
+    { "<leader>bd", "<Cmd>confirm bdelete<CR>",       desc = "Delete current buffer" },
     { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Close other buffers" },
-    { "<leader>ba", "<Cmd>bufdo bdelete<CR>", desc = "Close all buffers" },
+    { "<leader>ba", "<Cmd>confirm bufdo bdelete<CR>", desc = "Close all buffers" },
   }
 }
