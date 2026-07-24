18195595ebde283ece0842a2f4eaee3a0e68e2f6
skai273 <kinoshita116@gmail.com>
2026-07-24 18:18:33 +0900

    update nvim settings to add key for BufferLinePick

--- configs/nvim/.config/nvim/lua/plugins/bufferline.lua
+++ configs/nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -12,6 +12,7 @@ return {
   keys = {
     { "]b",         "<Cmd>BufferLineCycleNext<CR>",   desc = "Next buffer line" },
     { "[b",         "<Cmd>BufferLineCyclePrev<CR>",   desc = "Previous buffer line" },
+    { "<leader>bp", "<Cmd>BufferLinePick<CR>",        desc = "Pick buffer line" },
     { "<leader>bd", "<Cmd>confirm bdelete<CR>",       desc = "Delete current buffer" },
     { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Close other buffers" },
     { "<leader>ba", "<Cmd>confirm bufdo bdelete<CR>", desc = "Close all buffers" },
