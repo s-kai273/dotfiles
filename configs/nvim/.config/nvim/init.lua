10cad7eab7f23db34267f02bd55ab5374bb7f051
Shin Kinoshita <skinoshita@conoris.jp>
2026-08-20 10:22:00 +0900

    update nvim settings to install lazy.nvim if not installed

--- configs/nvim/.config/nvim/init.lua
+++ configs/nvim/.config/nvim/init.lua
@@ -43,23 +43,21 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if vim.fn.isdirectory(lazypath) == 0 then
-  vim.fn.mkdir(vim.fn.fnamemodify(lazypath, ":h"), "p")
-  local clone_output = vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--branch=stable",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
-
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
-    error("Failed to clone lazy.nvim:\n" .. clone_output)
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
   end
 end
-
 vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup("plugins")
 
 require("keymaps")
