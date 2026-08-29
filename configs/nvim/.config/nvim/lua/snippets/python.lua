f2d25c7348350224790295058e0b3bafb36b1314
skai273 <kinoshita116@gmail.com>
2026-08-29 20:35:37 +0900

    update nvim settings to add pytest snipet

--- configs/nvim/.config/nvim/lua/snippets/python.lua
+++ configs/nvim/.config/nvim/lua/snippets/python.lua
@@ -1,6 +1,8 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
 
 return {
   s("ii", {
@@ -9,4 +11,22 @@ return {
   s("mii", {
     t("map(int, input().split())"),
   }),
+  s("pytest", fmt([[
+import pytest
+
+@pytest.mark.parametrize(
+    "{}",
+    [
+        ({}),
+    ],
+)
+def {}({}):
+    {}
+]], {
+    i(1, "abc, expected"),
+    i(2),
+    i(3, "xxx"),
+    i(4, "abc, expected"),
+    i(0, "pass"),
+  })),
 }
