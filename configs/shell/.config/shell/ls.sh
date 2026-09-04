261a30daba87c5a17aeba42016fcf12c55f5c03d
Shin Kinoshita <skinoshita@conoris.jp>
2026-09-04 14:04:29 +0900

    update shell settings for ls and zsh being available on mac env

--- configs/shell/.config/shell/ls.sh
+++ configs/shell/.config/shell/ls.sh
@@ -1,3 +1,10 @@
-eval "$(dircolors)"
-alias ls='ls --color=auto'
-
+case "$(uname -s)" in
+  Darwin)
+    export CLICOLOR=1
+    export LSCOLORS=ExGxFxdaCxDaDahbadacec
+    ;;
+  Linux)
+    eval "$(dircolors)"
+    alias ls='ls --color=auto'
+    ;;
+esac
--- configs/shell/.config/shell/zsh.sh
+++ configs/shell/.config/shell/zsh.sh
@@ -1,8 +1,24 @@
 bindkey -v
 bindkey -M viins '^E' autosuggest-accept
 
-source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+case "$(uname -s)" in
+  Darwin)
+    if [ -n "${HOMEBREW_PREFIX:-}" ]; then
+      brew_prefix="$HOMEBREW_PREFIX"
+    elif [ -d /opt/homebrew ]; then
+      brew_prefix=/opt/homebrew
+    else
+      brew_prefix=/usr/local
+    fi
+    source "$brew_prefix/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+    source "$brew_prefix/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+    unset brew_prefix
+    ;;
+  Linux)
+    source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+    source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+    ;;
+esac
 
 autoload -Uz compinit
 compinit
